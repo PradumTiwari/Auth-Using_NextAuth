659dec562b99b0f165a0a3513959ad505acb0a69
Pradum <satendarpradum@gmail.com>
2025-01-03 23:37:20 +0530

    Introduce lib and segregrate NEXT_AUTH

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,6 @@
-import NextAuth from "next-auth";
-import  CredentialsProvider  from "next-auth/providers/credentials";
-import FacebookProvider from 'next-auth/providers/facebook'
-import GithubProvider from "next-auth/providers/github";
+import { NEXT_AUTH_CONFIG } from "@/app/lib/auth"
+import NextAuth from "next-auth"
 
-const handler=NextAuth({
-   providers:[
-   
-    CredentialsProvider({
-        name:'Email',
-        credentials:{
-            username:{label:'email',type:'text',placeholder:"username"},
-            password:{label:'password',type:'password',placeholder:"password"}
-        },
+const handler = NextAuth(NEXT_AUTH_CONFIG)
 
-        async authorize(credentails:any){
-            console.log(credentails);
-           //Here the validation logic goes and based on it 
-           //The data is return  
-           
-            return {
-                id:"user1",
-                name:"Hello"
-            };
-        },
-    }),
-
-   
-   ],
-   secret:process.env.AUTH_SECRET
-})
-
-export {handler as GET,handler as POST};
+export { handler as GET, handler as POST }
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,9 @@
+import { NEXT_AUTH_CONFIG } from "@/app/lib/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    const session=await getServerSession();
+    const session=await getServerSession(NEXT_AUTH_CONFIG);
     return NextResponse.json({
         session
     })

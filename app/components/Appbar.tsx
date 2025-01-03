7ebc02b3ac22f5043a22fe226d8b26090dfbcb90
Pradum <satendarpradum@gmail.com>
2025-01-03 22:48:21 +0530

    Get user Detail on server and the api route

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,18 +1,20 @@
 "use client"
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation"
 import React from 'react'
 
 const Appbar = () => {
-    const router=useRouter();
+    const session=useSession();
   return (
     <div>Main Components
         <button onClick={()=>{
             // router.push("/api/auth/signin")
             signIn();
         }} >SignIn</button>
+        {JSON.stringify(session)}
     </div>
   )
+
 }
 
 export default Appbar
